29b649d75169387470ee021b61e8debe227d7b44
Diaz1401 <reagor8161@outlook.com>
2022-01-14 15:49:37 +0700

    build: Add support for build specific branch

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+REPO_BRANCH="$1"
 LLVM_NAME="CAT"
 DIR="$(pwd ...)"
 BOT_MSG_URL="https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage"
@@ -7,8 +8,9 @@ TG_CHAT_ID="-1001180467256"
 BUILD_DATE="$(date +%Y%m%d)"
 BUILD_DAY="$(date "+%B %-d, %Y")"
 THREADS="$(nproc --all)"
-BRANCH="release/12.x"
 CUSTOM_FLAGS="LLVM_PARALLEL_COMPILE_JOBS=$THREADS LLVM_PARALLEL_LINK_JOBS=$THREADS CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3"
+# Select clang repository for build
+case "${REPO_BRANCH}" in "clang-12") BRANCH="release/12.x" ;; "clang-13") BRANCH="release/13.x" ;; "main") BRANCH="main" ;; esac
 tg_post_msg(){ curl -q -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="$1" &> /dev/null; }
 tg_post_build(){ curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" -F chat_id="$TG_CHAT_ID" -F "disable_web_page_preview=true" -F "parse_mode=html" -F caption="$3" &> /dev/null; }
 # Build LLVM
@@ -41,7 +43,7 @@ tg_post_msg "<b>$LLVM_NAME: Cloning clang repository. . .</b>"
 ./lfs.sh
 git config --global user.name Diaz1401
 git config --global user.email reagor8161@outlook.com
-git clone "https://Diaz1401:$GH_TOKEN@github.com/Diaz1401/clang.git"
+git clone "https://Diaz1401:$GH_TOKEN@github.com/Diaz1401/clang.git" --branch "$REPO_BRANCH" --single-branch
 pushd clang || exit
 git lfs install
 rm -rf ./*

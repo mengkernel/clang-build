4b065924ab6f0ec588a23687ace8c3561f997bcc
Diaz1401 <reagor8161@outlook.com>
2022-02-25 14:33:02 +0700

    build: Small fixes

--- build.sh
+++ build.sh
@@ -45,7 +45,7 @@ git config --global user.name Diaz1401
 git config --global user.email reagor8161@outlook.com
 tg_post_msg "<b>$LLVM_NAME: Cloning repository. . .</b>"
 git clone https://Diaz1401:$GITHUB_TOKEN@github.com/Diaz1401/clang.git -b main --single-branch
-cd clang; rm -rf *; git add .; cp -rf ../install/* .
+cd clang; rm -rf *; cp -rf ../install/* .
 # Generate archive
 tg_post_msg "<b>$LLVM_NAME: Generate release archive. . .</b>"
 cp ../zstd/programs/zstd .; time tar --use-compress-program='./zstd --ultra -22 -T0' -cf clang.tar.zst aarch64-linux-gnu bin lib share
@@ -57,5 +57,5 @@ git add md5sum.txt version.txt
 git commit -asm "Clang: $clang_version-$BUILD_DATE, Binutils: $binutils_ver"
 tg_post_msg "<b>$LLVM_NAME: Starting release to repository. . .</b>"
 git push origin main
-hub release create -a zstd-v1.5.2.tar.zst -a clang.tar.zst -m 'Clang-$clang_version-$BUILD_DATE' $BUILD_DATE
+hub release create -a zstd-v1.5.2.tar.zst -a clang.tar.zst -m "Clang-$clang_version-$BUILD_DATE" $BUILD_DATE
 tg_post_msg "<b>$LLVM_NAME: Toolchain released to <code>https://github.com/Diaz1401/clang/releases/tag/latest</code></b>"

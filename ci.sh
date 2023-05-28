7b7b5397cd3f26100213aa7fb3a47f0e06af8623
Diaz1401 <reagor8161@outlook.com>
2023-05-28 13:41:57 +0700

    ci: update notification message
    Signed-off-by: Diaz1401 <reagor8161@outlook.com>

--- ci.sh
+++ ci.sh
@@ -31,8 +31,8 @@ tg_post_build(){
 }
 
 # Build LLVM
-tg_post_msg "<pre>Date                : ${BUILD_DAY}</pre><pre>GitHub Action       : Toolchain compilation started . . .</pre>"
-tg_post_msg "<pre>GitHub Action       : Building LLVM . . .</pre>"
+tg_post_msg "<pre>                Date: ${BUILD_DAY}</pre><pre>       GitHub Action: Toolchain compilation started . . .</pre>"
+tg_post_msg "<pre>       GitHub Action: Building LLVM . . .</pre>"
 BUILD_START=$(date +"%s")
 TOTAL_START=$(date +"%s")
 ./build-llvm.py -s \
@@ -50,15 +50,15 @@ DIFF=$((BUILD_END - BUILD_START))
 
 # Check LLVM files
 if [ -f install/bin/clang ]; then
-  tg_post_msg "<pre>GitHub Action       : LLVM compilation finished ! ! !</pre><pre>Time taken          : $((DIFF / 60))m $((DIFF % 60))s</pre>"
+  tg_post_msg "<pre>       GitHub Action: LLVM compilation finished ! ! !</pre><pre>          Time taken: $((DIFF / 60))m $((DIFF % 60))s</pre>"
 else
-  tg_post_msg "<pre>GitHub Action       : LLVM compilation failed ! ! !</pre>"
-  tg_post_build build.log "<pre>GitHub Action       : LLVM build.log</pre>"
+  tg_post_msg "<pre>       GitHub Action: LLVM compilation failed ! ! !</pre>"
+  tg_post_build build.log "<pre>       GitHub Action: LLVM build.log</pre>"
   exit 1
 fi
 
 # Build binutils
-tg_post_msg "<pre>GitHub Action       : Building Binutils. . .</pre>"
+tg_post_msg "<pre>       GitHub Action: Building Binutils. . .</pre>"
 BUILD_START=$(date +"%s")
 ./build-binutils.py \
   -t aarch64 x86_64 \
@@ -68,10 +68,10 @@ DIFF=$((BUILD_END - BUILD_START))
 
 # Check Binutils files
 if [ -f install/bin/ld ]; then
-  tg_post_msg "<pre>GitHub Action       : Binutils compilation finished ! ! !</pre><pre>Time taken          : $((DIFF / 60))m $((DIFF % 60))s</pre>"
+  tg_post_msg "<pre>       GitHub Action: Binutils compilation finished ! ! !</pre><pre>          Time taken: $((DIFF / 60))m $((DIFF % 60))s</pre>"
 else
-  tg_post_msg "<pre>GitHub Action       : Binutils compilation failed ! ! !</pre>"
-  tg_post_build build.log "<pre>GitHub Action       : Binutils build.log</pre>"
+  tg_post_msg "<pre>       GitHub Action: Binutils compilation failed ! ! !</pre>"
+  tg_post_build build.log "<pre>       GitHub Action: Binutils build.log</pre>"
   exit 1
 fi
 
@@ -95,15 +95,15 @@ rm .file-idx
 # Clone GitHub repository
 CLANG_VERSION="$(install/bin/clang --version | head -n1 | cut -d ' ' -f4)"
 BINUTILS_VERSION="$(install/bin/ld --version | head -n1 | cut -d ' ' -f5)"
-tg_post_msg "<pre>GitHub Action       : Toolchain compilation Finished</pre><pre>Clang Version       : ${CLANG_VERSION}</pre><pre>Binutils Version    : ${BINUTILS_VERSION}</pre>"
+tg_post_msg "<pre>       GitHub Action: Toolchain compilation Finished</pre><pre>Clang Version       : ${CLANG_VERSION}</pre><pre>Binutils Version    : ${BINUTILS_VERSION}</pre>"
 git config --global user.name Diaz1401
 git config --global user.email reagor8161@outlook.com
-tg_post_msg "<pre>GitHub Action       : Cloning repository. . .</pre>"
+tg_post_msg "<pre>       GitHub Action: Cloning repository. . .</pre>"
 git clone https://Diaz1401:${GITHUB_TOKEN}@github.com/Diaz1401/clang.git -b main --single-branch
 cd clang; rm -rf *; cp -rf ../install/* .
 
 # Generate archive
-tg_post_msg "<pre>GitHub Action       : Generate release archive. . .</pre>"
+tg_post_msg "<pre>       GitHub Action: Generate release archive. . .</pre>"
 cp ../zstd .; tar --use-compress-program='./zstd -12' -cf clang.tar.zst aarch64-linux-gnu bin lib share
 
 # Push to GitHub repository
@@ -112,10 +112,10 @@ echo "${BUILD_DATE} build, Clang: ${CLANG_VERSION}, Binutils: ${BINUTILS_VERSION
 git checkout README.md
 git commit --allow-empty -as \
   -m "Clang: ${CLANG_VERSION}-${BUILD_DATE}, Binutils: ${BINUTILS_VERSION}" --
-tg_post_msg "<pre>GitHub Action       : Starting release to repository. . .</pre>"
+tg_post_msg "<pre>       GitHub Action: Starting release to repository. . .</pre>"
 git push origin main
 hub release create -a clang.tar.zst -m "Clang-${CLANG_VERSION}-${BUILD_DATE}" ${BUILD_DATE}
-tg_post_msg "<pre>GitHub Action       : Toolchain released to https://github.com/Diaz1401/clang/releases/latest</pre>"
+tg_post_msg "<pre>       GitHub Action: Toolchain released to https://github.com/Diaz1401/clang/releases/latest</pre>"
 TOTAL_END=$(date +"%s")
 DIFF=$((TOTAL_END - TOTAL_START))
-tg_post_msg "<pre>Total CI operation  : $((DIFF / 60))m $((DIFF % 60))</pre>"
+tg_post_msg "<pre>  Total CI operation: $((DIFF / 60))m $((DIFF % 60))</pre>"

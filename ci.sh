f26ea5712bedb3581f0914b0c04217278399b1ae
Diaz1401 <diaznura@outlook.com>
2024-04-07 04:00:46 +0700

    Revert "ci: Enable BOLT"
    This reverts commit 1e8acda532466cea1db49fd36598f222fc222464.

--- ci.sh
+++ ci.sh
@@ -45,8 +45,6 @@ build_llvm(){
     --install-folder "${INSTALL}" \
     --lto thin \
     --pgo llvm \
-    --bolt \
-    --assertions \
     --projects clang lld polly \
     --shallow-clone \
     --targets AArch64 X86 \

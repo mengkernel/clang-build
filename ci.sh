a0a159fe7da7ffb8d2043ae9aa5852ca59e189f3
Diaz1401 <diaznura@outlook.com>
2024-06-23 19:26:41 +0700

    Revert "ci: Specify install targets"
    This reverts commit 3f455c201ce778e89a7db679a45ed11a61afbcdd.

--- ci.sh
+++ ci.sh
@@ -69,7 +69,6 @@ build_llvm() {
     --build-stage1-only \
     --defines "${CUSTOM_FLAGS}" \
     --install-folder "${INSTALL}" \
-    --install-targets clang lld polly \
     --lto thin \
     --pgo llvm \
     --bolt \

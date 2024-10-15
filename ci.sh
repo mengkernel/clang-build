125498ea65a648518486b10fa9cac7d4ee988f72
Diaz1401 <diaznura@outlook.com>
2024-10-15 15:22:53 +0700

    ci: LLVM 19.1.1
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- ci.sh
+++ ci.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export LLVM_NAME="kucing"
-export STABLE_TAG="llvmorg-18.1.8"
+export STABLE_TAG="llvmorg-19.1.1"
 export INSTALL="${PWD}/install"
 export CHAT_ID="-1001180467256"
 export BUILD_DATE="$(date +%Y%m%d)"
